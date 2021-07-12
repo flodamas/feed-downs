d16227a1ef9e2ce28f590894f88757423adcc1f8
Florian Damas <florian.damas@cern.ch>
2021-07-12 18:04:01 +0200

    add branching fractions

---
+++ data/branching.C
@@ -0,0 +1,118 @@
+/*
+ Numerical values of branching ratios within the bottomonium family
+
+ from PDG 2021 https://pdg.lbl.gov/2021/listings/contents_listings.html
+*/
+
+/// Dimuon branching ratios (in %)
+
+Float_t br1Stomumu = 2.48;
+Float_t syst1Stomumu = 0.05;
+
+Float_t br2Stomumu = 1.93;
+Float_t syst2Stomumu = 0.17;
+
+Float_t br3Stomumu = 2.18;
+Float_t syst3Stomumu = 0.21;
+
+/// Y(3S) decays
+
+// to Y(2S)
+Float_t br3Sto2Sanything = 10.6;
+Float_t syst3Sto2Sanything = 0.8;
+
+Float_t br3Sto2Spipluspiminus = 2.82;
+Float_t syst3Sto2Spipluspiminus = 0.18;
+
+Float_t br3Sto2Stwopizero = 1.85;
+Float_t syst3Sto2Stwopizero = 0.14;
+
+Float_t br3Sto2Stwophotons = 5.0;
+Float_t syst3Sto2Stwophotons = 0.7;
+
+// to Y(1S)
+Float_t br3Sto1Spipluspiminus = 4.37;
+Float_t syst3Sto1Spipluspiminus = 0.08;
+
+Float_t br3Sto2Stwopizero = 2.20;
+Float_t syst3Sto2Stwopizero = 0.13;
+
+// to chi_b(2P)
+Float_t br3Sto2P_Jequal2 = 13.1;
+Float_t syst3Sto2P_Jequal2 = 1.6;
+
+Float_t br3Sto2P_Jequal1 = 12.6;
+Float_t syst3Sto2P_Jequal1 = 1.2;
+
+Float_t br3Sto2P_Jequal0 = 5.9;
+Float_t syst3Sto2P_Jequal0 = 0.6;
+
+// to chi_b(1P)
+Float_t br3Sto1P_Jequal2 = 1.0;
+Float_t syst3Sto1P_Jequal2 = 0.1;
+
+Float_t br3Sto1P_Jequal1 = 9e-2;
+Float_t syst3Sto1P_Jequal1 = 5e-2;
+
+Float_t br3Sto1P_Jequal0 = .27;
+Float_t syst3Sto1P_Jequal0 = .04;
+
+/// Y(2S) decays
+
+// to Y(1S)
+Float_t br2Sto1Spipluspiminus = 17.85;
+Float_t syst2Sto1Spipluspiminus = 0.26;
+
+Float_t br2Sto2Stwopizero = 8.6;
+Float_t syst2Sto2Stwopizero = 0.4;
+
+// to chi_b(1P)
+Float_t br2Sto1P_Jequal2 = 6.9;
+Float_t syst2Sto1P_Jequal2 = 0.4;
+
+Float_t br2Sto1P_Jequal1 = 7.15;
+Float_t syst2Sto1P_Jequal1 = 0.35;
+
+Float_t br2Sto1P_Jequal0 = 3.8;
+Float_t syst2Sto1P_Jequal0 = 0.4;
+
+/// Chi_b(3P) radiative decays observed but no branching franctions available in the PDG
+
+// use feed-down fractions measured at high pt by LHCb? (7 and 8 TeV)
+
+/// Chi_b(2P) radiative decays (branching fractions in %)
+
+// J = 2
+Float_t br2P_Jequal2_to2Sgamma = 8.9;
+Float_t syst2P_Jequal2_to2Sgamma = 1.2;
+
+Float_t br2P_Jequal2_to1Sgamma = 6.6;
+Float_t syst2P_Jequal2_to1Sgamma = 0.8;
+
+// J = 1
+Float_t br2P_Jequal1_to2Sgamma = 18.1;
+Float_t syst2P_Jequal1_to2Sgamma = 1.9;
+
+Float_t br2P_Jequal1_to1Sgamma = 9.9;
+Float_t syst2P_Jequal1_to1Sgamma = 1.0;
+
+// J = 0
+Float_t br2P_Jequal0_to2Sgamma = 1.38;
+Float_t syst2P_Jequal0_to2Sgamma = 0.30;
+
+Float_t br2P_Jequal0_to1Sgamma = 0.38;
+Float_t syst2P_Jequal0_to1Sgamma = 0.17;
+
+/// Chi_b(1P) radiative decays (branching fractions in %)
+
+// J = 2
+Float_t br1P_Jequal2_to1Sgamma = 18.0;
+Float_t syst1P_Jequal2_to1Sgamma = 1.0;
+
+// J = 1
+Float_t br1P_Jequal1_to1Sgamma = 35.2;
+Float_t syst1P_Jequal1_to1Sgamma = 2.0;
+
+// J = 0
+Float_t br1P_Jequal0_to1Sgamma = 1.94;
+Float_t syst1P_Jequal0_to1Sgamma = 0.27;
--- myPlot.C
+++ myPlot.C
@@ -1,73 +1,80 @@
-TCanvas *myCanvas(const char *name) {
-  auto *c = new TCanvas(name, "", 700, 600);
-  c->SetFillColor(0);
-  return c;
+TCanvas* myCanvas(const char* name) {
+	auto* c = new TCanvas(name, "", 700, 600);
+	c->SetFillColor(0);
+	return c;
 }
 
 const Float_t xErrorWidth = 0.5;
 
-TGraphErrors *myStatGraph(const char *title, Int_t nPoints, Float_t x[],
+TGraphErrors* myStatGraph(const char* title, Int_t nPoints, Float_t xBinning[],
                           Float_t y[], Float_t yErr[], Color_t color = kRed,
                           Int_t markerStyle = 20) {
+	Float_t x[nPoints], xErr[nPoints];
 
-  Float_t xErr[nPoints];
+	for (Int_t i = 0; i < nPoints; i++) {
+		x[i] = (xBinning[i + 1] + xBinning[i]) / 2;
+		xErr[i] = (xBinning[i + 1] - xBinning[i]) / 2;
+	}
 
-  for (Int_t i = 0; i < nPoints; i++)
-    xErr[i] = 0;
+	auto* graph = new TGraphErrors(nPoints, x, y, xErr, yErr);
 
-  auto *graph = new TGraphErrors(nPoints, x, y, xErr, yErr);
+	graph->SetTitle(title);
+	graph->SetMarkerStyle(markerStyle);
+	graph->SetMarkerColor(color);
+	graph->SetLineColor(color); // for error bars
+	graph->SetLineWidth(2);
 
-  graph->SetTitle(title);
-  graph->SetMarkerStyle(markerStyle);
-  graph->SetMarkerColor(color);
-  graph->SetLineColor(color); // for error bars
-  graph->SetLineWidth(2);
-  graph->SetMinimum(0.);
-  graph->SetMaximum(50);
+	graph->SetMinimum(0);
+	graph->SetMaximum(50);
 
-  graph->GetXaxis()->SetLimits(0, 40);
+	graph->GetXaxis()->SetLimits(0, 40);
 
-  return graph;
+	return graph;
 }
 
-TGraphErrors *mySystGraph(Int_t nPoints, Float_t x[], Float_t y[],
+TGraphErrors* mySystGraph(Int_t nPoints, Float_t xBinning[], Float_t y[],
                           Float_t yErr[], Color_t color = kRed) {
-  Float_t xErr[nPoints];
+	Float_t x[nPoints], xErr[nPoints];
 
-  for (Int_t i = 0; i < nPoints; i++)
-    xErr[i] = xErrorWidth;
+	for (Int_t i = 0; i < nPoints; i++) {
+		x[i] = (xBinning[i + 1] + xBinning[i]) / 2;
+		xErr[i] = xErrorWidth;
+	}
 
-  auto *graph = new TGraphErrors(nPoints, x, y, xErr, yErr);
+	auto* graph = new TGraphErrors(nPoints, x, y, xErr, yErr);
 
-  graph->SetFillStyle(0);
-  graph->SetLineColor(color); // box
+	graph->SetLineWidth(0);
+	graph->SetFillStyle(1001);
+	graph->SetLineColor(color); // box
+	//graph->SetFillColor(color);
+	graph->SetFillColorAlpha(color, .4);
 
-  return graph;
+	return graph;
 }
 
-void drawHeaderLegend(const char *text, Float_t x = 0.2, Float_t y = 0.9) {
-  TLatex *header = new TLatex();
-  header->SetTextAlign(12);
+void drawHeaderLegend(const char* text, Float_t x = 0.2, Float_t y = 0.9) {
+	TLatex* header = new TLatex();
+	header->SetTextAlign(12);
 
-  header->DrawLatexNDC(x, y, text);
+	header->DrawLatexNDC(x, y, text);
 }
 
-void drawLegend(TObject *graph, const char *text, Float_t x, Float_t y = .84,
-                const char *opt = "p") {
-  TLegend *legend = new TLegend(x, y + 0.03, x + 0.2, y - 0.03);
-  // legend->SetTextAlign(12);
-  legend->SetFillStyle(0);
-  legend->AddEntry(graph, text, opt);
+void drawLegend(TObject* graph, const char* text, Float_t x, Float_t y = .84,
+                const char* opt = "p") {
+	TLegend* legend = new TLegend(x, y + 0.03, x + 0.2, y - 0.03);
+	// legend->SetTextAlign(12);
+	legend->SetFillStyle(0);
+	legend->AddEntry(graph, text, opt);
 
-  legend->Draw();
+	legend->Draw();
 }
 
-TGraph *myGraph(Int_t nPoints, Float_t x[], Float_t y[], Color_t color = kBlack,
+TGraph* myGraph(Int_t nPoints, Float_t x[], Float_t y[], Color_t color = kBlack,
                 Int_t lineStyle = 1) {
-  TGraph *graph = new TGraph(nPoints, x, y);
-  graph->SetLineWidth(2);
-  graph->SetLineColor(color);
-  graph->SetLineStyle(lineStyle);
+	TGraph* graph = new TGraph(nPoints, x, y);
+	graph->SetLineWidth(2);
+	graph->SetLineColor(color);
+	graph->SetLineStyle(lineStyle);
 
-  return graph;
+	return graph;
 }
